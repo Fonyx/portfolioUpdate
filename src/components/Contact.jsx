a6e7709ec651f4314d63fda394698e0431c2d24c
Nick <nick.alex.ritchie@gmail.com>
2021-10-17 17:05:28 +1100

    text colors all sorted

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,15 +3,15 @@ import M from 'materialize-css';
 import {Row, Col, Button} from 'react-materialize'
 import '../App.css'
 
-function Contact() {
+function Contact({textColor}) {
     return (
-        <div>
+        <div className={`${textColor}`}>
             <h3>Contact</h3>
-           <h2>Come buy me a coffee and we can chat</h2>
+            <h2>Come buy me a coffee and we can chat</h2>
 
-           <a href="mailto:nick.alex.ritchie@gmail.com">
-               <Button className='secondary_bg' waves='purple'>nick.alex.ritchie@gmail.com</Button>
-           </a>
+            <a href="mailto:nick.alex.ritchie@gmail.com">
+                <Button className={`quaternary_bg ${textColor}`} waves='orange'>nick.alex.ritchie@gmail.com</Button>
+            </a>
         </div>
     )
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,12 +3,12 @@ import M from 'materialize-css';
 import {Row, Col, Button} from 'react-materialize'
 import '../App.css'
 
-export default function Resume() {
+export default function Resume({textColor}) {
     return (
-        <div>
+        <div className={`${textColor}`}>
             <h3>Resume</h3>
             <a href="https://fonyx.github.io/resume/" target="_blank">
-                <Button className="secondary_bg" waves='purple'>Resume
+                <Button className={`quaternary_bg ${textColor}`} waves='orange'>Resume
                 </Button>
             </a>
             <h3>Skills</h3>
