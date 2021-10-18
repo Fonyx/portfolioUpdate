7b91f9ba7d0fd64d5ade5125ae071e0002665dc7
Nick <nick.alex.ritchie@gmail.com>
2021-10-18 16:19:05 +1100

    Updates

---
+++ precache-manifest.379aadcce0acc64c795455d0093ac830.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56581fe2868686dd7c446b6302c3f179",
+    "url": "/portfolioUpdate/index.html"
+  },
+  {
+    "revision": "5cec638fab1e55b32332",
+    "url": "/portfolioUpdate/static/css/2.a389d3b1.chunk.css"
+  },
+  {
+    "revision": "e2a3cb253ee6234a5260",
+    "url": "/portfolioUpdate/static/css/main.191e3340.chunk.css"
+  },
+  {
+    "revision": "5cec638fab1e55b32332",
+    "url": "/portfolioUpdate/static/js/2.4c750db3.chunk.js"
+  },
+  {
+    "revision": "72fd59532eb17178206ab23098d256db",
+    "url": "/portfolioUpdate/static/js/2.4c750db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115ec5c737455cbda35b",
+    "url": "/portfolioUpdate/static/js/3.bdf4dcee.chunk.js"
+  },
+  {
+    "revision": "e2a3cb253ee6234a5260",
+    "url": "/portfolioUpdate/static/js/main.7a81957c.chunk.js"
+  },
+  {
+    "revision": "0c3a81fa040cc7d5e949",
+    "url": "/portfolioUpdate/static/js/runtime-main.bcd25e3c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Fonyx/portfolioUpdate/precache-manifest.a1577803a2d0710fa7f490e2795ba021.js"
+  "/portfolioUpdate/precache-manifest.379aadcce0acc64c795455d0093ac830.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fonyx/portfolioUpdate/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolioUpdate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
