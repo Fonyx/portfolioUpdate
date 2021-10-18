4a0a7e59cdeb7a68aa0bb38903a40c26bf570b5d
Nick <nick.alex.ritchie@gmail.com>
2021-10-18 12:54:12 +1100

    first acceptable particles

--- src/App.js
+++ src/App.js
@@ -50,17 +50,115 @@ function App() {
           <main>
               <Particles
                 params={{
-                  particles: {
-                    number: {
-                      value: 100,
-                      density: {
-                        enable: true,
-                        value_area: 1000,
+                  "particles": {
+                    "number": {
+                      "value": 80,
+                      "density": {
+                        "enable": true,
+                        "value_area": 800
                       }
                     },
+                    "color": {
+                      "value": "#554067"
+                    },
+                    "shape": {
+                      "type": "polygon",
+                      "stroke": {
+                        "width": 0,
+                        "color": "#f0ab2c"
+                      },
+                      "polygon": {
+                        "nb_sides": 6
+                      },
+                      "image": {
+                        "src": "img/github.svg",
+                        "width": 100,
+                        "height": 100
+                      }
+                    },
+                    "opacity": {
+                      "value": 0.06313280775270874,
+                      "random": true,
+                      "anim": {
+                        "enable": false,
+                        "speed": 0.39957485235709195,
+                        "opacity_min": 0.1,
+                        "sync": false
+                      }
+                    },
+                    "size": {
+                      "value": 11.83740145363289,
+                      "random": true,
+                      "anim": {
+                        "enable": false,
+                        "speed": 40,
+                        "size_min": 0.1,
+                        "sync": false
+                      }
+                    },
+                    "line_linked": {
+                      "enable": true,
+                      "distance": 150,
+                      "color": "#554067",
+                      "opacity": 0.4,
+                      "width": 1
+                    },
+                    "move": {
+                      "enable": true,
+                      "speed": 2,
+                      "direction": "none",
+                      "random": false,
+                      "straight": false,
+                      "out_mode": "out",
+                      "bounce": false,
+                      "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                      }
+                    }
                   },
-                }}
-              />
+                  "interactivity": {
+                    "detect_on": "canvas",
+                    "events": {
+                      "onhover": {
+                        "enable": true,
+                        "mode": "bubble"
+                      },
+                      "onclick": {
+                        "enable": true,
+                        "mode": "push"
+                      },
+                      "resize": true
+                    },
+                    "modes": {
+                      "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                          "opacity": 1
+                        }
+                      },
+                      "bubble": {
+                        "distance": 266.04729729729706,
+                        "size": 12.668918918918909,
+                        "duration": 0.9290540540540533,
+                        "opacity": 0.6249999999999996,
+                        "speed": 3
+                      },
+                      "repulse": {
+                        "distance": 200,
+                        "duration": 0.4
+                      },
+                      "push": {
+                        "particles_nb": 4
+                      },
+                      "remove": {
+                        "particles_nb": 2
+                      }
+                    }
+                  },
+                  "retina_detect": true
+                }}/>
             <Container>
               {renderPage()}
             </Container>
