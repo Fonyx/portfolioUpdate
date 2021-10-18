739b90c8a4cb943c8c0d03501321003652bb4703
Nick <nick.alex.ritchie@gmail.com>
2021-10-18 16:11:29 +1100

    Updates

---
+++ precache-manifest.a1577803a2d0710fa7f490e2795ba021.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b60e84293ba9735b77e0d5abc2556f",
+    "url": "/Fonyx/portfolioUpdate/index.html"
+  },
+  {
+    "revision": "5cec638fab1e55b32332",
+    "url": "/Fonyx/portfolioUpdate/static/css/2.a389d3b1.chunk.css"
+  },
+  {
+    "revision": "e2a3cb253ee6234a5260",
+    "url": "/Fonyx/portfolioUpdate/static/css/main.191e3340.chunk.css"
+  },
+  {
+    "revision": "5cec638fab1e55b32332",
+    "url": "/Fonyx/portfolioUpdate/static/js/2.4c750db3.chunk.js"
+  },
+  {
+    "revision": "72fd59532eb17178206ab23098d256db",
+    "url": "/Fonyx/portfolioUpdate/static/js/2.4c750db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115ec5c737455cbda35b",
+    "url": "/Fonyx/portfolioUpdate/static/js/3.bdf4dcee.chunk.js"
+  },
+  {
+    "revision": "e2a3cb253ee6234a5260",
+    "url": "/Fonyx/portfolioUpdate/static/js/main.7a81957c.chunk.js"
+  },
+  {
+    "revision": "339924e3f11707b211e6",
+    "url": "/Fonyx/portfolioUpdate/static/js/runtime-main.542820a7.js"
+  }
+]);
