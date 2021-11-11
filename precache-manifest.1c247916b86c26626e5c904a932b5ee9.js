1b972342f9b6e58dea52dedf35e45d3e0bae1e62
Nick <nick.alex.ritchie@gmail.com>
2021-11-11 17:58:10 +1100

    Updates

--- precache-manifest.1c247916b86c26626e5c904a932b5ee9.js
+++ precache-manifest.1c247916b86c26626e5c904a932b5ee9.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c8bafc8f023cb54fa4088c839d3e12e",
+    "revision": "adae642fd62e85937ff22705d8af1cd6",
     "url": "/portfolioUpdate/index.html"
   },
   {
-    "revision": "6d5d8f796458354686ab",
+    "revision": "17f999eabac8df4f2d0d",
     "url": "/portfolioUpdate/static/css/2.a389d3b1.chunk.css"
   },
   {
-    "revision": "3dec0f276c83a08342f5",
-    "url": "/portfolioUpdate/static/css/main.60603655.chunk.css"
+    "revision": "5fdb6b2cc45c4438ce6e",
+    "url": "/portfolioUpdate/static/css/main.07e82c87.chunk.css"
   },
   {
-    "revision": "6d5d8f796458354686ab",
-    "url": "/portfolioUpdate/static/js/2.70c3ba47.chunk.js"
+    "revision": "17f999eabac8df4f2d0d",
+    "url": "/portfolioUpdate/static/js/2.2ae2bed6.chunk.js"
   },
   {
     "revision": "72fd59532eb17178206ab23098d256db",
-    "url": "/portfolioUpdate/static/js/2.70c3ba47.chunk.js.LICENSE.txt"
+    "url": "/portfolioUpdate/static/js/2.2ae2bed6.chunk.js.LICENSE.txt"
   },
   {
     "revision": "aa73910472846b1a5047",
     "url": "/portfolioUpdate/static/js/3.7ffff9e0.chunk.js"
   },
   {
-    "revision": "3dec0f276c83a08342f5",
-    "url": "/portfolioUpdate/static/js/main.b8c7f007.chunk.js"
+    "revision": "5fdb6b2cc45c4438ce6e",
+    "url": "/portfolioUpdate/static/js/main.aa6d731a.chunk.js"
   },
   {
     "revision": "79d227f32313fc07959a",
